1c7c4c09f43b084431147012afc10bd3d0f092df
cstmateuzx <jennings1808@gmail.com>
2025-10-24 18:56:08 -0300

    Corrige deploy para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/disclosure_page/',
+  base: '/disclosure_page/', // 👈 coloque o nome EXATO do repositório
 })
